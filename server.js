425746bf935771eb06ae8ac11128b97654ff8382
Nathan Andersen <nathan.andersens@gmail.com>
2016-05-31 09:24:12 -0400

    Debugging.

--- server.js
+++ server.js
@@ -58,26 +58,37 @@ app.post("/playlists",function(req,res) {
   var newPlaylist = req.body;
   newPlaylist.createDate = new Date();
 
-  console.log("made it here");
+  console.log("0");
   if (req.body.name == null) {
     handleError(res, "Invalid user input","Did not provide a name",400);
+    return;
   }
+  console.log("1");
   if (req.body.playlistId == null) {
     handleError(res, "Invalid user input", "Did not provide a playlist ID",400);
-    }
+    return;
+  }
+  console.log("2");
   if (req.body.hostId == null) {
     handleError(res, "Invalid user input", "Did not provide a host ID",400);
+    return;
   }
+  console.log("3");
   if (req.body.hostIsLoggedInToSpotify == null) {
     handleError(res, "Invalid user input", "Did not provide Spotify login status",400);
+    return;
   }
+  console.log("4");
   if (req.body.hostIsLoggedInToSoundcloud == null) {
     handleError(res, "Invalid user input", "Did not provide Soundcloud login status",400);
+    return;
   }
+  console.log("5");
   if (req.body.hostIsLoggedInToAppleMusic == null) {
     handleError(res, "Invalid user input", "Did not provide Apple Music login status",400);
+    return;
   }
-  console.log("shabang");
+  console.log("6");
 
   db.collection(PLAYLISTS_COLLECTION).insertOne(newPlaylist, function(err,doc) {
     if (err) {
