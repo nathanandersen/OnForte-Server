20de6c480cee15b2c7a2a847949b0ae88620576e
Nathan Andersen <nathan.andersens@gmail.com>
2016-06-01 11:40:07 -0400

    A first commit towards a website.

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+angular.module("contactsApp", ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+              templateUrl: "home.html",
+              controller: "HomeController"
+            })
+            .when ("/:playlistId", {
+              controller: "PlaylistController",
+              templateUrl: "playlist.html",
+              resolve {
+                songs: function(Songs) {
+                  return Songs.getSongsByPlaylist();
+                }
+              }
+            })
+            .otherwise({
+                redirectTo: "/"
+            })
+    })
+    .service("Songs", function($http) {
+      this.getSongsByPlaylist = function(playlistId) {
+        var url = "/playlistsongs/" + playlistId;
+        return $http.get(url).
+          then(function(response) {
+              return response;
+          }, function(response) {
+            alert("Error getting the songs.");
+          });
+      }
+    })
+    .controller("HomeController", function($scope) {
+      console.log("Hello, world");
+    })
+    .controller("PlaylistController", function($scope, $routeParams) {
+      Songs.getSongsByPlaylist($routeParams.playlistId).then(function(docs) {
+        console.log(docs);
+      })
+    });
